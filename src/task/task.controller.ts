bcf7149839ce11d65f010f3d56ffbb417125e90b
Bruce Neco <bruce.neco3@outlook.com>
2020-05-28 10:39:30 -0300

    bronze challenge done

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe, ParseIntPipe, Query, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe, ParseIntPipe, Query, NotFoundException, Put, Logger, Delete } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,14 +7,19 @@ import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @Controller('tasks')
 export class TaskController {
-    constructor(private taskService: TaskService) {}
+    constructor(private taskService: TaskService) { }
 
     @Get()
-    @ApiOperation({ summary: 'Get all Tasks', description: 'buscas todas as tarefas cadastradas no banco de dados'})
+    @ApiOperation({ summary: 'Get all Tasks', description: 'buscas todas as tarefas cadastradas no banco de dados' })
     @ApiResponse({ status: 200, description: 'ok', type: Task, isArray: true })
-    async getTasks(): Promise<Task[]> {
+    async getTasks(@Query('completed') completed): Promise<Task[]> {
+        if (completed) {
+            completed = completed.toLowerCase() === 'true';
+            return await this.taskService.getIsComplete(completed);
+        }
         return await this.taskService.getTasks();
     }
+
 
     @Get('/:id')
     @ApiOperation({ summary: 'Get specific Task', description: 'buscas uma tarefas cujo ID seja igual ao informado na requisição' })
@@ -24,11 +29,23 @@ export class TaskController {
         return await this.taskService.getById(id);
     }
 
+    @Put('/:id')
+    @ApiOperation({ summary: 'Update a task by ID', description: 'atualiza a task que coincidir com a ID passada no parâmetro' })
+    async updateTaskById(@Body() updateTask: CreateTaskDto, @Param('id') id): Promise<Task> {
+        return await this.taskService.updateTaskById(updateTask, id);
+    }
+
     @Post()
     @UsePipes(ValidationPipe)
     @ApiOperation({ summary: 'Create a Task', description: 'cria uma nova tarefa e registra no banco de dados' })
     @ApiResponse({ status: 200, description: 'ok', type: Task, isArray: false })
     async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
         return await this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    @ApiOperation({ summary: 'Delete a Task', description: 'deleta uma tarefa do banco de dados' })
+    async deleteTask(@Param('id') id) {
+        return await this.taskService.deleteTask(id);
     }
 }
