1f1c166a240552f65449f09bf06adbb48da4aaaf
Caio <caio.rodrigues@datarevlabs.com>
2020-05-30 17:43:54 -0300

    Missing entity import

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Car } from './car.entity';
+import { GetCarFilterDto } from './dto/filter-car.dto';
+import { CarService } from './car.service';
+import { Controller, Get, ValidationPipe, Query } from '@nestjs/common';
 
 @Controller('car')
-export class CarController {}
+export class CarController {
+    constructor(private carService: CarService) {}
+
+    @Get()
+    async getCars(@Query(ValidationPipe) filterDto: GetCarFilterDto): Promise<Car[]> {
+        return await this.carService.getCars(filterDto);
+    }
+}
