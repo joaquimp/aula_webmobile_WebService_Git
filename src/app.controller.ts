90aa304e79e890cbad860b18fb635623a15a3373
Daniel52x <danielcost0705@gmail.com>
2020-05-31 21:52:01 -0300

    Authentication - Ouro

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService, private readonly appService: AppService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 
   @Get()
   getHello(): any {
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { CarController } from './car/car.controller';
 import { CarModule } from './car/car.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),TaskModule, CarModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),TaskModule, CarModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
