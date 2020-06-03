33261daea4af66425afe72fe763cd1b0c8f9d8d4
Estevan Fonseca <estevanoliveira21@gmail.com>
2020-06-03 19:26:22 -0300

    Bronze

--- src/task/task.repository.ts
+++ src/task/task.repository.ts
@@ -1,4 +1,4 @@
-import { Repository, EntityRepository } from "typeorm";
+import { Repository, EntityRepository,  } from "typeorm";
 import { Task } from "./task.entity";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
@@ -7,9 +7,14 @@ import { GetTasksFilterDto } from "./dto/get-task-filter.dto";
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
 
-    async getTask(): Promise<Task[]> {
+    async getTask(completed: boolean): Promise<Task[]> {
         
         const query = this.createQueryBuilder('task');
+        if(completed == true){
+            query.where("status = :type", { type: 'DONE' });
+        } else {
+            query.where("status != :type", { type: 'DONE' });
+        }
         const tasks = await query.getMany();
         return tasks;
     }
@@ -20,8 +25,22 @@ export class TaskRepository extends Repository<Task> {
         task.description = createTaskDto.description;
         task.status = TaskStatus.OPEN;
         await task.save();
-        
         return task;
+    }
+
+    async updateTask(id: number, createTaskDto: CreateTaskDto): Promise<Task> {
+        const query = this.createQueryBuilder('task')
+        .update(createTaskDto).where('id = :id', {id: id});
+
+        await query.execute();
+        return this.findOne(id);
+    }
+
+    async deleteTask(id: number): Promise<number> {
+        const query = this.createQueryBuilder('task')
+        .delete().where('id = :id', {id: id});
+        
+        return (await query.execute()).affected;
     }
 
 }
